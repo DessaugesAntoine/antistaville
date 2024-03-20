07c7b01e82485696ab143274111128ee01c6f4a4
Dessauges Antoine <dessauges.antoine@gmail.com>
2024-03-20 11:51:05 +0100

    News Single : Desktop + responsive

---
+++ src/components/HighlightedLinkComponent.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+        props: ['text', 'link']
+    }
+</script>
+
+<template>
+    <a href="{{ link }}" class="text-custom-green underline underline-offset-2 decoration-custom-green transition hover:bg-bgd-green/20">{{ text }}</a>
+</template>
